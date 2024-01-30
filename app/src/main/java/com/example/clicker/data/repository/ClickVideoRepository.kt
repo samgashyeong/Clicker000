6470c5dc97ad791cb308b60e741e1dd0c9c8adc0
이준상 <66434787+samgashyeong@users.noreply.github.com>
2024-01-30 19:26:12 +0900

    TypeConverter 추가

--- app/src/main/java/com/example/clicker/data/repository/ClickVideoRepository.kt
+++ app/src/main/java/com/example/clicker/data/repository/ClickVideoRepository.kt
@@ -1,8 +1,34 @@
 package com.example.clicker.data.repository
 
 import android.app.Application
+import com.example.clicker.data.database.ClickVideoListWithClickInfo
+import com.example.clicker.data.database.room.ClickVideoDao
 import com.example.clicker.data.database.room.ClickVideoDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class ClickVideoRepository(application: Application) {
-    private val clickVideoDatabase : ClickVideoDatabase = ClickVideoDatabase.getInstance(application)!!
+    private lateinit var clickVideoDatabase : ClickVideoDatabase
+    private lateinit var clickVideoDao : ClickVideoDao
+    private lateinit var clickVideos : List<ClickVideoListWithClickInfo>
+
+    init {
+        GlobalScope.launch(Dispatchers.IO) {
+            clickVideoDatabase = ClickVideoDatabase.getInstance(application)!!
+            clickVideoDao = clickVideoDatabase.clickVideoDao()
+            clickVideos = clickVideoDao.getAll()
+        }
+    }
+    fun getAll(): List<ClickVideoListWithClickInfo> {
+        return clickVideos
+    }
+
+    fun insert(clickVideoListWithClickInfo: ClickVideoListWithClickInfo){
+        try {
+            GlobalScope.launch(Dispatchers.IO) {
+                clickVideoDao.insert(clickVideoListWithClickInfo)
+            }
+        }catch (e: Exception){}
+    }
 }
