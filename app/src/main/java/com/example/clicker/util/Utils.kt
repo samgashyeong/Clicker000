f7d23460870504b8d0c46884a09770982841994a
이준상 <66434787+samgashyeong@users.noreply.github.com>
2024-09-02 17:07:23 +0900

    [Feat] Util 파일 추가

---
+++ app/src/main/java/com/example/clicker/util/Utils.kt
@@ -0,0 +1,24 @@
+package com.example.clicker.util
+
+import com.example.clicker.viewmodel.Mode
+
+object Util {
+
+    const val youtubeUrl = "https://youtube.googleapis.com/youtube/v3/"
+}
+
+val intToMode : Map<Int, Mode> = mapOf(
+    0 to Mode.Default(),
+    1 to Mode.Ranking()
+)
+val modeToInt : Map<Mode, Int> = mapOf(
+    Mode.Default() to 0,
+    Mode.Ranking() to 1
+)
+
+data class RankingDto(
+    val name : String,
+    val plus : Int,
+    val minus : Int,
+    val total : Int
+)
